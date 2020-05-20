36a9f3f8005e8da614e98346bd04d5a61c69c1b2
ponka123 <anka.irhina@gmail.com>
2020-05-20 12:55:46 -0700

    Updates

---
+++ precache-manifest.6195d73b91fbc0dc62a4eb6a44b4d542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a89e517bc0fcb9a33c1ca943db217f",
+    "url": "/github.io/react_app_1/index.html"
+  },
+  {
+    "revision": "9ec74a4a38e6a67927d3",
+    "url": "/github.io/react_app_1/static/css/main.4c8ce262.chunk.css"
+  },
+  {
+    "revision": "4b0d0af9a4e74368f258",
+    "url": "/github.io/react_app_1/static/js/2.c7ddb900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react_app_1/static/js/2.c7ddb900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec74a4a38e6a67927d3",
+    "url": "/github.io/react_app_1/static/js/main.e2fb5b81.chunk.js"
+  },
+  {
+    "revision": "ac31c9b99d07c5edeffc",
+    "url": "/github.io/react_app_1/static/js/runtime-main.adfbaa08.js"
+  }
+]);
